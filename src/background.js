82adcf59415f05b673c5c4f047e65ad7e20b2f90
Lucas Baldin <lbaldin@ciandt.com>
2021-12-09 12:05:52 -0300

    added sheet url

--- src/background.js
+++ src/background.js
@@ -1,10 +1,24 @@
-const API_KEY = "";
+const API_KEY = "AIzaSyCJ_vChC1Da_TnhAG3qnBPcNTttxmuYiko";
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.message === "get_auth_token") {
     chrome.identity.getAuthToken({ interactive: true }, function (token) {
       console.log("back");
       console.log(token);
+    });
+  } else if (request.message === "spread_sheet") {
+    chrome.identity.getAuthToken({ interactive: true }, function (token) {
+      const fetch_url = `https://sheets.googleapis.com/v4/spreadsheets/1EEivp2ETb-SvN5S3fKt8Cg7qyP9aasmlArtrKzNtuL4/values/A1?key=${API_KEY}`;
+      const fetch_options = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        method: 'GET'
+      };
+
+      fetch(fetch_url, fetch_options)
+        .then((res) => res.json())
+        .then((res) => console.log(res));
     });
   }
 });
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 document.getElementById("oauth-button").addEventListener("click", function () {
   alert("oi");
   console.log("front");
-  chrome.runtime.sendMessage({ message: "get_auth_token" });
+  // chrome.runtime.sendMessage({ message: "get_auth_token" });
+  chrome.runtime.sendMessage({ message: "spread_sheet" });
 });
