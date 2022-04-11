114bfb0f1f2698cc3bfc1df64fa49d4d5ce43899
Mariana Campos <marianacristinadecampos@gmail.com>
2022-04-11 12:14:33 -0300

    creating function to get the current date

---
+++ src/js/date.js
@@ -0,0 +1,9 @@
+function getCurrentDate(){
+    currentDate = new Date();
+    document.getElementById('date') = (addZero(currentDate.getFullYear()).toString()) + "-" + (addZero(currentDate.getMonth()+1).toString()) + "-" + (addZero(currentDate.getDate().toString()));
+}
+
+function addZero(number){
+    if (number <= 9) return "0" + number;
+    return number; 
+}
