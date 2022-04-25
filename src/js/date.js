2dd1b001b1af78ee0828e241d1e9e39d426df4ff
Mariana Campos <marianacristinadecampos@gmail.com>
2022-04-25 12:21:21 -0300

    feat(task/#11): adding logic to get current date when user clicks on body

--- src/js/date.js
+++ src/js/date.js
@@ -1,9 +1,11 @@
-function getCurrentDate(){
-    currentDate = new Date();
-    document.getElementById('date') = (addZero(currentDate.getFullYear()).toString()) + "-" + (addZero(currentDate.getMonth()+1).toString()) + "-" + (addZero(currentDate.getDate().toString()));
-}
+const body = document.getElementsByTagName('body');
 
-function addZero(number){
+body[0].addEventListener("click", function () {
+  currentDate = new Date();
+  document.getElementById('date').value = (addZero(currentDate.getFullYear()).toString()) + "-" + (addZero(currentDate.getMonth()+1).toString()) + "-" + (addZero(currentDate.getDate().toString()));
+
+  function addZero(number){
     if (number <= 9) return "0" + number;
     return number; 
-}
+  }
+})
