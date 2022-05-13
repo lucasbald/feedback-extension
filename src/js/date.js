fe77fd8906d795b205881e71d4d9df0ecc241cfa
Mariana Campos <marianacristinadecampos@gmail.com>
2022-05-13 11:23:28 -0300

    refactor: refactor code

--- src/js/date.js
+++ src/js/date.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   document.getElementById('date').value = (addZero(currentDate.getFullYear()).toString()) + "-" + (addZero(currentDate.getMonth()+1).toString()) + "-" + (addZero(currentDate.getDate().toString()));
 
   function addZero(number){
-    if (number <= 9) return "0" + number;
+    if (number <= 9) return `0${number}`;
     return number; 
   }
 })
