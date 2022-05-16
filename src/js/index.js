c4803250d3888d4e9d78fc6d80369904def1673a
Lucas Baldin <lbaldin@ciandt.com>
2022-05-16 11:50:21 -0300

    Fixed url button not sharing the input text url

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-document.getElementById("proceed").addEventListener("submit", () => {
+document.getElementById("proceed").addEventListener("click", () => {
   const spreadsheetURL = document.getElementById("spreadsheetURL").value;
 
   chrome.runtime.sendMessage({ message: 'post_spreadsheet_url', spreadsheetURL });
