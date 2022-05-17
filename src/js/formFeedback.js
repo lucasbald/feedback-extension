328e827001599cba5a6598ccd4e149d7aeb82a79
Carla Ponciano <cponciano@ciandt.com>
2022-05-16 23:17:19 -0300

    [task/#15] Saved data to chrome storage

--- src/js/formFeedback.js
+++ src/js/formFeedback.js
@@ -1,3 +1,11 @@
+const fieldsName = [
+  'people',
+  'action',
+  'cluster',
+  'reference',
+  'date'
+];
+
 document.getElementById("feedbackForm").addEventListener("submit", function () {
   const fields = {
     people: document.getElementById("people").value,
@@ -8,4 +16,25 @@ document.getElementById("feedbackForm").addEventListener("submit", function () {
   }
 
   chrome.runtime.sendMessage({ message: 'post_form', fields});
+  chrome.storage.sync.remove(fieldsName);
 })
+
+document.addEventListener("DOMContentLoaded", () => {
+  for (let fieldName of fieldsName) {
+    let field = document.getElementById(fieldName);
+    field.oninput = updateData;
+  }
+
+  chrome.storage.sync.get(elements => {
+    for (let fieldName of fieldsName) {
+      if (elements[fieldName] !== undefined) {
+        let field = document.getElementById(fieldName);
+        field.value = elements[fieldName];
+      }
+    }
+  });
+});
+
+function updateData(event) {
+  chrome.storage.sync.set({[this.id]: this.value});
+}
